cfd781639e70bc1ee05eff7da29e507482739de6
Thiago Henrique PC <tiagohenrriquebuenodeOliveira@gmail.com>
2022-05-12 12:00:42 -0300

    fontes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -18,6 +18,10 @@ export const theme = extendTheme({
             
         }
     },
+    fonts: {
+        heading: 'Roboto',
+        body: 'Roboto'
+    },
     styles: {
         global: {
             body:{
