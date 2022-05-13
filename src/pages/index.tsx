cd0a819736f8732a2c979e7bc76aaae83d0dae16
Thiago Henrique PC <tiagohenrriquebuenodeOliveira@gmail.com>
2022-05-13 11:36:55 -0300

    Fazendo o input um componente.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex // isso é uma div
       w="100vw"
@@ -18,38 +18,8 @@ export default function Home() {
         flexDirection="column" // pra fazer os input ficar um abaixo do ouotro
       >
         <Stack spacing='4'>
-          <FormControl>
-
-
-            <FormLabel htmlFor='email'>E-mail</FormLabel>
-            <Input
-              name='email'
-              id='email'
-              type='email'
-              focusBorderColor='pink.500'
-              bgColor='gray.900'
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor='password'>password</FormLabel>
-            <Input
-              name='password'
-              id='password'
-              type='password'
-              focusBorderColor='pink.500'
-              bgColor='gray.900'
-              variant="filled"
-              _hover={{
-                bgColor: 'regray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name='email' type='email' label='E-mail' />
+          <Input name='password' type='password' label='Senha' />
         </Stack>
         <Button type='submit' mt='6' colorScheme='pink' size='lg'>Entrar</Button>
       </Flex>
