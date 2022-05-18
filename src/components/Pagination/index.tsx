5fc7c18fd98f56334c75622dba991056db01ea70
Thiago Henrique PC <tiagohenrriquebuenodeOliveira@gmail.com>
2022-05-18 12:06:49 -0300

    responsividade na lista de usuário.

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -5,7 +5,7 @@ import { PaginationItem } from "./PaginationItem";
 export function Pagination() {
     return (
         <Stack
-            direction='row'
+            direction={['column','row']}
             spacing='6'
             mt='8'
             justify='space-between'
@@ -20,7 +20,6 @@ export function Pagination() {
                 <PaginationItem number={3} />
                 <PaginationItem number={4} />
                 <PaginationItem number={5} />
-   
             </Stack>
         </Stack>
     );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpoint, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -6,6 +6,12 @@ import { Sidebar } from "../../components/Sidebar";
 
 
 export default function UserList() {
+    
+    const isWideVersion = useBreakpointValue({
+        base:false,
+        lg: true,
+    })
+    
     return (
         <Box>
             <Header />
@@ -28,17 +34,17 @@ export default function UserList() {
                     <Table colorScheme='whiteAlpha'>
                         <Thead>
                             <Tr>
-                                <Th px='6' color='gray.300' w='8'>
+                                <Th px={['4', '4', '6']} color='gray.300' w='8'>
                                     <Checkbox colorScheme='pink' />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                                { isWideVersion && (<Th>Data de cadastro</Th>)}
                                 <Th w='8'></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px='6'>
+                                <Td px={['4', '4', '6']}>
                                     <Checkbox colorScheme='pink' />
                                 </Td>
                                 <Td>
@@ -47,8 +53,8 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'>thiagoHe@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>08 de janeiro, 2023</Td>
-                                <Td>
+                                { isWideVersion && (<Td>08 de janeiro, 2023</Td>)}
+                                { isWideVersion && (<Td>
                                     <Button as='a'
                                         size='sm'
                                         fontSize='sm'
@@ -56,10 +62,10 @@ export default function UserList() {
                                         leftIcon={<Icon as={RiPencilLine} fontSize='15' />}
                                     >
                                         Editar</Button>
-                                </Td>
+                                </Td>)}
                             </Tr>
                             <Tr>
-                                <Td px='6'>
+                                <Td px={['4', '4', '6']}>
                                     <Checkbox colorScheme='pink' />
                                 </Td>
                                 <Td>
@@ -68,8 +74,8 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'>Clara@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>08 de janeiro, 2023</Td>
-                                <Td>
+                                { isWideVersion && (<Td>08 de janeiro, 2023</Td>)}
+                                { isWideVersion && (<Td>
                                     <Button as='a'
                                         size='sm'
                                         fontSize='sm'
@@ -77,10 +83,10 @@ export default function UserList() {
                                         leftIcon={<Icon as={RiPencilLine} fontSize='15' />}
                                     >
                                         Editar</Button>
-                                </Td>
+                                </Td>)}
                             </Tr>
                             <Tr>
-                                <Td px='6'>
+                                <Td px={['4', '4', '6']}>
                                     <Checkbox colorScheme='pink' />
                                 </Td>
                                 <Td>
@@ -89,8 +95,8 @@ export default function UserList() {
                                         <Text fontSize='sm' color='gray.300'>Marcelo@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>10 de janeiro, 2023</Td>
-                                <Td>
+                                { isWideVersion &&  (<Td>10 de janeiro, 2023</Td>)}
+                                { isWideVersion && (<Td>
                                     <Button as='a'
                                         size='sm'
                                         fontSize='sm'
@@ -98,7 +104,7 @@ export default function UserList() {
                                         leftIcon={<Icon as={RiPencilLine} fontSize='15' />}
                                     >
                                         Editar</Button>
-                                </Td>
+                                </Td>)}
                             </Tr>
                         </Tbody>
                     </Table>
