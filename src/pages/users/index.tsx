f84bcb8219182769611c85c5f7699b5b45ca0fd0
Thiago Henrique PC <tiagohenrriquebuenodeOliveira@gmail.com>
2022-05-31 11:07:41 -0300

    Atualizando

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,7 +10,7 @@ import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
 
-    const { data, isLoading, error } = useQuery('users', async () => {
+    const { data, isLoading, error, isFetching } = useQuery('users', async () => {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
 
@@ -27,7 +27,9 @@ export default function UserList() {
             }
         })
 
-        return users
+        return users    
+    }, {
+        staleTime: 1000 * 5,
     })
 
 
@@ -46,7 +48,10 @@ export default function UserList() {
 
                 <Box flex='1' borderRadius={8} bg='gray.800' padding='8'>
                     <Flex mb='8' justify='space-between' align='center'>
-                        <Heading size='lg' fontWeight='normal' >Usuários</Heading>
+                        <Heading size='lg' fontWeight='normal'>
+                            Usuários
+                            { !isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4' />}
+                            </Heading>
 
                         <Link href='/users/create' passHref>
                             <Button as='a'
