803b4a4b227657932ff8f576e11f2815603b3077
Thiago Henrique <tiagohenrriquebuenodeoliveira@gmail.com>
2022-06-04 09:59:20 -0300

    Update index.ts

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model, Response } from 'miragejs'
+import { createServer, Factory, Model } from 'miragejs'
 
 import * as faker from '@faker-js/faker';
 
@@ -30,35 +30,15 @@ export function makeServer() {
         },
 
         seeds(server) {
-            server.createList('user', 200)
+            server.createList('user', 10)
         },
         
         routes() {
-            this.namespace = 'dash-go.herokuapp.com'
+            this.namespace = 'api'
             this.timing = 750
 
 
-            this.get('/users', function(schema, request){
-                const { page = 1, per_page = 10 } =  request.queryParams
-
-                const total = schema.all('user').length
-
-                // page = 3
-                // 3 - 1 = 2 * 10 = 20
-                // começa no 20
-                const pageStart = (Number(page) - 1) * Number(per_page);
-                const pageEnd = pageStart + Number(per_page)
-
-                const users = this.serialize(schema.all('user'))
-                    .users.slice(pageStart, pageEnd)
-
-                return new Response(
-                    200,
-                    {'x-total-count': String(total)},
-                    {users}
-                )
-
-            })
+            this.get('/users')
             this.post('/users')
 
             this.namespace = ''
