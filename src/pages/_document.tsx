d9cc55690dd39d4965c4577dc4906afe9e44628d
Thiago Henrique <tiagohenrriquebuenodeoliveira@gmail.com>
2022-06-07 11:59:38 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,22 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
-                    <Script src="/hotjar.js"></Script>
+                    <script
+            dangerouslySetInnerHTML={{
+              __html:`
+               <!-- Hotjar Tracking Code for https://dash-go.herokuapp.com -->
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3006502,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `
+            }}
+          />
+                    
                 </Head>
                 <body>
                     <Main />
