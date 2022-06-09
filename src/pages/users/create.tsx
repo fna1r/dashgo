e4065cf26858bcf50d39264441f016eaa8496d85
Thiago Henrique <tiagohenrriquebuenodeoliveira@gmail.com>
2022-06-09 10:45:11 -0300

    Update create.tsx

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,9 +1,9 @@
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, Stack } from "@chakra-ui/react";
 import Link from "next/link";
+import Head from "next/head";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
-
 import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -41,6 +41,24 @@ export default function CreateUser() {
     }
 
     return (
+        <>
+                    <Head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html:`
+               <!-- Hotjar Tracking Code for https://dash-go.herokuapp.com -->
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3006502,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `
+            }}
+          />
+        </Head>
         <Box>
             <Header />
             <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
@@ -107,5 +125,6 @@ export default function CreateUser() {
                 </Box>
             </Flex>
         </Box>
+            </>
     );
 }
