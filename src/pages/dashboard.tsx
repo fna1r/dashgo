65e074b4e69e6dc62349ce2eb0132bd3168462bd
Thiago Henrique <tiagohenrriquebuenodeoliveira@gmail.com>
2022-06-09 11:05:06 -0300

    Update dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,10 +2,29 @@ import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+import Head from "next/head";
 
 
 export default function Dashboard() {
     return (
+        <>
+            <Head>
+        <script
+            dangerouslySetInnerHTML={{
+              __html:`
+               <!-- Hotjar Tracking Code for https://dash-go.herokuapp.com -->
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3006502,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `
+            }}
+          />
+        </Head>
         <Flex direction='column'>
             <Header />
             <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
@@ -14,5 +33,6 @@ export default function Dashboard() {
                 test
             </Flex>
         </Flex>
+           </>
     )
 }
